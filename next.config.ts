6e007fbbccaa1dade3790f22cba5b0c080ca6040
idenis88 <hexed88@gmail.com>
2026-04-21 23:48:07 +0200

    ignore eslint during build

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import createNextIntlPlugin from 'next-intl/plugin';
 
 const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
 
-const nextConfig = {};
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
 
 export default withNextIntl(nextConfig);
