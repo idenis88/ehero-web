eb4b648fbf027ca7317a8fd79a5cd9b3d41a2f26
idenis88 <hexed88@gmail.com>
2026-04-22 00:12:26 +0200

    add routes

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,35 +1,7 @@
-import Nav from '../../../components/layout/Nav';
-import Footer from '../../../components/layout/Footer';
-import Hero from '../../../components/sections/Hero';
-import Marquee from '../../../components/sections/Marquee';
-import WhyFeatures from '../../../components/sections/WhyFeatures';
-import HowItWorks from '../../../components/sections/HowItWorks';
-import Audience from '../../../components/sections/Audience';
-import StripeThriveCart from '../../../components/sections/StripeThriveCart';
-import WhyEhero from '../../../components/sections/WhyEhero';
-import Testimonials from '../../../components/sections/Testimonials';
-import Pricing from '../../../components/sections/Pricing';
-import FAQ from '../../../components/sections/FAQ';
-import CTA from '../../../components/sections/CTA';
-
-export default function MinimaxPage() {
+export default function HomePage() {
   return (
-    <>
-      <Nav />
-      <main>
-        <Hero />
-        <Marquee />
-        <HowItWorks />
-        <WhyFeatures />
-        <Audience />
-        <StripeThriveCart />
-        <WhyEhero />
-        <Testimonials />
-        <Pricing />
-        <FAQ />
-        <CTA />
-      </main>
-      <Footer />
-    </>
+    <main>
+      <h1>Home page - coming soon</h1>
+    </main>
   );
 }
